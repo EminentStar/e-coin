738c66ca7da19c663b90c11299be03a7f9651752
EminentStar <junk3843@naver.com>
2017-05-26 00:02:05 +0900

    Create CoinAccount model
    CoinAccount model will be used for storing users' coin and money that is
    used for recharging coins.

--- e-coin-jkpark/ecoin/models.py
+++ e-coin-jkpark/ecoin/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
+class CoinAccount(models.Model):
+    """This model has users' coin and money amount."""
+    real_money = models.IntegerField(default=0)
+    ecoin = models.IntegerField(default=0)
+    username = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
