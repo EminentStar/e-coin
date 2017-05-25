954f67b26a51769b1be7f3bcad82a8595417d03e
EminentStar <junk3843@naver.com>
2017-05-26 08:07:05 +0900

    Implement sample scraper for purchasing products
    I implement sample scraper which scraps product data(product name, price) in naver shopping page. That's because I could construct a product list on view and users can virtually buy the products with e-coin.

---
+++ e-coin-jkpark/ecoin/scraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+SHOPPING_URL='http://shopping.naver.com/search/all.nhn?query='
+
+def scrap_searched_page(query, base_url=SHOPPING_URL): 
+    """This function scraps product data and returns a product list."""
+    url = base_url + query 
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'lxml')
+    product_tags = soup.findAll('li', {'class': '_product_list'})
+    
+    products = []
+
+    for p in product_tags:
+        title = p.find('a', {'class': 'tit'})
+        price = p.find('span', {'class':'num _price_reload'})
+
+        title = title.text.strip()
+        price = int(price.text.strip().replace(',',''))
+        products.append({'title': title, 'price': price})
+    
+    return products
