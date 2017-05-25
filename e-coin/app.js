daaaba704fe2bf21de8e7a6e624823d3cff67f58
Changwoo <chawoo.jeon@gmail.com>
2017-05-25 16:39:57 +0900

    기본 구조 나누기

--- e-coin/app.js
+++ e-coin/app.js
@@ -5,14 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,15 +26,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(3000, () => {
+  console.log(`Server is running on 3000 port`);
 });
-
-module.exports = app;
--- e-coin/routes/index.js
+++ e-coin/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const api = require('./api');
+const public = require('./public');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use('/api', api);
+router.use('/public', public);
+
+router.get('/', (req, res) => {
+  console.log('route');
+  res.send();
 });
 
 module.exports = router;
