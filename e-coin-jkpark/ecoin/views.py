326f63e16ba41e034ef695cef87db9d627b62b35
EminentStar <junk3843@naver.com>
2017-05-25 16:41:39 +0900

    Do formation Web UI
    Using bootstrap, I create base html file including templates.

--- e-coin-jkpark/ecoin/views.py
+++ e-coin-jkpark/ecoin/views.py
@@ -4,8 +4,9 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.http import require_GET
 from django.views.decorators.http import require_POST
 
-# Create your views here.
 
 @require_GET
 def index(request):
-    return JsonResponse({'version': 'v1'}, status=200)
+    # return JsonResponse({'version': 'v1'}, status=200)
+    rendered_values = {'test_val': 'For Test'}
+    return render(request, 'ecoin/main_view.html', rendered_values)
