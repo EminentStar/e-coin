0b70ddc5d8bddb4e247edc8ba5a36792313a9a6b
EminentStar <junk3843@naver.com>
2017-05-26 12:58:50 +0900

    Create MoneyRechargement, CoinRechargement models

--- e-coin-jkpark/ecoin/models.py
+++ e-coin-jkpark/ecoin/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from datetime import datetime
+
 
 # Create your models here.
 class CoinAccount(models.Model):
@@ -14,9 +16,25 @@ class CoinAccount(models.Model):
 
 
 class Purchase(models.Model):
-    """This model has the information about each purchase of users."""
+    """This model has the information about each product purchase of users."""
     username = models.CharField(max_length=150, null=False)
     product_name = models.CharField(max_length=255, null=False)
     product_count = models.IntegerField(default=0)
     ecoin_price = models.IntegerField(default=0)
+    purchased_time = models.DateTimeField(default=datetime.now(), null=False)
 
+
+class MoneyRechargement(models.Model):
+    """This model has the information about each money rechargement of users."""
+    username = models.CharField(max_length=150, null=False)
+    recharged_money = models.IntegerField(default=0) 
+    recharged_time = models.DateTimeField(default=datetime.now(), null=False)
+
+
+class CoinRechargement(models.Model):
+    """This model has the information about each ecoin rechargement of users."""
+    username = models.CharField(max_length=150, null=False)
+    used_money = models.IntegerField(default=0)
+    recharged_ecoin = models.IntegerField(default=0)
+    recharged_time = models.DateTimeField(default=datetime.now(), null=False)
+    
