a52fd725c664e8b8bf0922f6e8eb06c373a5e54e
Changwoo <chawoo.jeon@gmail.com>
2017-05-29 16:39:32 +0900

    중복된 폴더 제거

--- changwoo/changwoo/routes/index.js
+++
@@ -1,9 +0,0 @@
-const api = require('./api');
-const public = require('./public');
-const express = require('express');
-const router = express.Router();
-
-router.use('/api', api);
-router.use('/public', public);
-
-module.exports = router;
--- changwoo/changwoo/routes/public/index.js
+++
@@ -1,9 +0,0 @@
-const { user } = require('../../app/public');
-const express = require('express');
-const router = express.Router();
-
-router.post('/users', user.createUser);
-router.get('/users/:id', user.getUser);
-router.post('/users/login', user.loginUser);
-
-module.exports = router;
